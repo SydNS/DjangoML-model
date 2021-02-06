c15474b136d592e182e707f6a73269685c3e62ad
root <root@localhost.localdomain>
2021-02-06 04:57:34 -0500

    added a register for the model in the admin file for it to be seen in the admin dashbod

--- SaleML_PreDjango/Predicting/admin.py
+++ SaleML_PreDjango/Predicting/admin.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
-
+from predict.models import Product
 # Register your models here.
+
+admin.site.register(Product)
