77423e3d082f7214bf6a00fc1d8270c73ba8ffd3
root <root@localhost.localdomain>
2021-02-06 04:56:09 -0500

    added a product description function

--- SaleML_PreDjango/Predicting/views.py
+++ SaleML_PreDjango/Predicting/views.py
@@ -20,3 +20,29 @@ def operate_function(product_detail):
                                                                         battery, price, pre_release_demand
                                                                         )
     return cluster_assigned[0], int(predicted_sales[0])
+
+
+
+def product_describe_view(request):
+    """
+    View to take the data from the user and process year
+    """
+    product_added = False
+    cluster_assigned = 0
+    predicted_sales = 0
+    if request.method == 'POST':
+        product_form = ProductForm(data=request.POST)
+        if product_form.is_valid():
+            product_detail = product_form.save()
+            product_added = True
+            print(product_detail)
+            cluster_assigned, predicted_sales = operate_function(product_detail)
+            print("Cluster Assigned : {}".format(cluster_assigned))
+            product_detail.save()
+    # Not a HTTP POST, so we render our form using two ModelForm instances.
+    # These forms will be blank, ready for user input.
+    else:
+        product_form = ProductForm()
+    return render(request, 'predict/add_product.html',
+                  {'product_form': product_form, 'product_added': product_added, 'cluster_assigned': cluster_assigned,
+                   'predicted_sales': predicted_sales})
